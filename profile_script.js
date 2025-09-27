c94f1a209e2dcd9da8b1e9786b89922ea349f8fe
spacerival <chantal.le21003@gmail.com>
2025-09-26 23:39:53 -0700

    update code

---
+++ profile_script.js
@@ -0,0 +1,36 @@
+const toggle = document.getElementById("toggle");
+const overview_button = document.getElementById("overview_button");
+const spending_button = document.getElementById("spending_button");
+const budget_button = document.getElementById("budget_button");
+
+const overview_section = document.getElementById("overview_section");
+const spending_section = document.getElementById("spending_section");
+const budget_section = document.getElementById("budget_section");
+
+toggle.addEventListener("click", function() {
+    document.getElementById("sidebar").classList.toggle("collapsed");
+});
+
+overview_button.addEventListener("click", () => {
+    overview_section.style.display = "grid";
+    spending_section.style.display= "none";
+    budget_section.style.display = "none";
+
+});
+
+spending_button.addEventListener("click", () => {
+    spending_section.style.display= "grid";
+    overview_section.style.display = "none";
+    budget_section.style.display = "none";
+
+});
+
+budget_button.addEventListener("click", () => {
+    budget_section.style.display = "grid";
+    overview_section.style.display = "none";
+    spending_section.style.display= "none";
+
+});
+
+
+
