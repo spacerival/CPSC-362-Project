1bfe8742c13beed18799844bda39fc90d27a27ee
spacerival <chantal.le21003@gmail.com>
2025-10-08 23:37:35 -0700

    adding transaction tab

--- profile_script.js
+++ profile_script.js
@@ -2,10 +2,12 @@ const toggle = document.getElementById("toggle");
 const overview_button = document.getElementById("overview_button");
 const spending_button = document.getElementById("spending_button");
 const budget_button = document.getElementById("budget_button");
+const transaction_button = document.getElementById("transaction_button");
 
 const overview_section = document.getElementById("overview_section");
 const spending_section = document.getElementById("spending_section");
 const budget_section = document.getElementById("budget_section");
+const transaction_section = document.getElementById("transaction_section");
 
 toggle.addEventListener("click", function() {
     document.getElementById("sidebar").classList.toggle("collapsed");
@@ -15,6 +17,7 @@ overview_button.addEventListener("click", () => {
     overview_section.style.display = "grid";
     spending_section.style.display= "none";
     budget_section.style.display = "none";
+    transaction_section.style.display = "none";
 
 });
 
@@ -22,6 +25,7 @@ spending_button.addEventListener("click", () => {
     spending_section.style.display= "grid";
     overview_section.style.display = "none";
     budget_section.style.display = "none";
+    transaction_section.style.display = "none";
 
 });
 
@@ -29,8 +33,13 @@ budget_button.addEventListener("click", () => {
     budget_section.style.display = "grid";
     overview_section.style.display = "none";
     spending_section.style.display= "none";
+    transaction_section.style.display = "none";
 
 });
 
-
-
+transaction_button.addEventListener("click", () => {
+    transaction_section.style.display = "grid";
+    overview_section.style.display = "none";
+    spending_section.style.display= "none";
+    budget_section.style.display = "none";
+})
